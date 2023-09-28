aecdf2854f1245c56466b3adb9fc59c41f36ce6d
Tyler Dodson <tsdodson@uark.edu>
2023-09-28 13:01:41 -0500

    connected entry screen to database

--- database.py
+++ database.py
@@ -5,6 +5,9 @@ from supabase import create_client, Client
 from faker import Faker
 import faker_commerce
 
+os.environ["SUPABASE_URL"] = "https://bkxlgoiyqsmvsykhgopu.supabase.co"
+os.environ["SUPABASE_KEY"] = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJreGxnb2l5cXNtdnN5a2hnb3B1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQ2NTIyNzIsImV4cCI6MjAxMDIyODI3Mn0.N64l3Prio2ucUYXv65UBhq_wxa0lVk62_00nGvCeH38"
+
 
 def add_entries(supabase, id, first_name,last_name,codename):
     value = {'id': id, 'first_name': first_name, 'last_name': last_name, 'codename': codename}
@@ -18,13 +21,12 @@ def update_entries(supabase, newid, previd, first_name, last_name, codename):
 
 
 
-def main():
-    vendor_count = 10
-    load_dotenv()
-    url: str = os.environ.get("SUPABASE_URL")
-    key: str = os.environ.get("SUPABASE_KEY")
-    supabase: Client = create_client(url, key)
-    #add_entries(supabase, 45, 'Tyler', 'Dodson', 'Dotty')
-    update_entries(supabase, 46, 45, 'Joe', 'Bergin', 'pomelo')
+# def main():
+#     vendor_count = 10
+#     load_dotenv()
+#     url: str = os.environ.get("SUPABASE_URL")
+#     key: str = os.environ.get("SUPABASE_KEY")
+#     supabase: Client = create_client(url, key)
+#     add_entries(supabase, 57, "Jim", "Beam", "Whiskey")
 
-main()
+# main()
