ab55a0f3d6e91196dfc956f27c41934115efcbbd
Joseph Bergin <josephsbergin@gmail.com>
2023-09-28 17:24:31 -0500

    updated player entry screen

--- player_entry_screen.py
+++ player_entry_screen.py
@@ -13,58 +13,90 @@ key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
 
 class PlayerEntry:
+
+    def createEntries(self):
+        for row in range(15):
+            label = tk.Label(self.frame, text=f"{row + 1}:")
+            label.grid(row=row, column=0, sticky="e")
+
+            entry = tk.Entry(self.frame)
+            entry.grid(row=row, column=1)
+
+            entry2 = tk.Entry(self.frame)
+            entry2.grid(row=row, column=2)
+
+            entry3 = tk.Entry(self.frame)
+            entry3.grid(row=row, column=3)
+
+            self.team1ID.append(entry)
+            self.team1CodeName.append(entry2)
+            self.team1EquipmentID.append(entry3)
+
+        for row in range(15):
+            label = tk.Label(self.frame, text=f"{row + 1}:")
+            label.grid(row=row, column=4, sticky="e")
+
+            entry = tk.Entry(self.frame)
+            entry.grid(row=row, column=5)
+
+            entry2 = tk.Entry(self.frame)
+            entry2.grid(row=row, column=6)
+
+            entry3 = tk.Entry(self.frame)
+            entry3.grid(row=row, column=7)
+
+            self.team2ID.append(entry)
+            self.team2CodeName.append(entry2)
+            self.team2EquipmentID.append(entry3)
+        return
+    
+    def getInputs(self):
+        self.team1Entries = []
+        self.team2Entries = []
+        for i in range(0,15):
+            self.team1Entries.append([[self.team1ID[i].get()], [self.team1CodeName[i].get()], [self.team1EquipmentID[i].get()]])
+            self.team2Entries.append([[self.team2ID[i].get()], [self.team2CodeName[i].get()], [self.team2EquipmentID[i].get()]])
+        print(self.team1Entries, self.team2Entries)
+        return 
+    
+    def add_player(self):
+        self.getInputs()
+
+        # add_entries(supabase,id,firstname,lastname,codename)
+        # transmitEquipmentCode(id) # Change to equipment code
+        return
+
     def __init__(self):
         self.root = tk.Tk()
-        self.root.title('Software Gurus')
+        self.root.geometry("800x600")
+        self.frame = tk.Frame(self.root)
+        self.frame.pack(pady=20)
+        self.root.title('Software Gurus - Laser Tag')
+        
+        self.team1ID = []
+        self.team1CodeName = []
+        self.team1EquipmentID = []
 
-        # Create a label widget
-        label = tk.Label(self.root, text="Enter your first name:")
+        self.team2ID = []
+        self.team2CodeName = []
+        self.team2EquipmentID = []
 
-        # Create a text box widget
-        self.firstname_entry = tk.Entry(self.root)
+        # Create labels and entries
+        self.createEntries()
 
-        label1 = tk.Label(self.root, text="Enter your last name:")
-        self.lastname_entry = tk.Entry(self.root)
-        label2 = tk.Label(self.root, text="Enter your codename:")
-        self.codename_entry = tk.Entry(self.root)
-
-        display_database = tk.Button(self.root,text="Display database", command=self.display_database())
-        # Create a button widget
-        button = tk.Button(self.root, text="Add Player", command=self.add_player)
-        # removebutton = tk.Button(self.root,text="Remove Player", command=self.remove_player)
-        self.remove_entry = tk.Entry(self.root)
-        # label3 = tk.Label(self.root,text="Remove a player:")
-
-        # Add the widgets to the root window
-        label.pack()
-        self.firstname_entry.pack()
-        label1.pack()
-        self.lastname_entry.pack()
-        label2.pack()
-        self.codename_entry.pack()
-        button.pack()
-        display_database.pack()
+        # Button when clicked, retrieves all information filled out
+        add_player = tk.Button(self.root, text="Add Players", command=self.add_player)
+        add_player.pack()
 
         # Start the main event loop
         self.root.mainloop()
-
-    def add_player(self):
-        id = random.randint(10,99)
-        firstname = self.firstname_entry.get()
-        lastname = self.lastname_entry.get()
-        codename = self.codename_entry.get()
-        add_entries(supabase,id,firstname,lastname,codename)
-        transmitEquipmentCode(id) # Change to equipment code
-        
-    # def remove_player(self, id):
-    #     id = self.remove_entry.get()
-
-    def display_database(self):
-        print(supabase.table('player').select("*").execute())
 
 
 
 # Create an instance of the NameGUI class
 gui = PlayerEntry()
+
+
+
 
 
