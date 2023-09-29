ecad135705e823f6d5ea7cd0112b15f1963d1f8b
William <williambfinck@gmail.com>
2023-09-29 14:32:46 -0500

    build now runs Splash as script

--- build.py
+++ build.py
@@ -4,8 +4,16 @@ import multiprocessing
 # Define the names of the files you want to run
 file1 = "udpserver.py"
 file2 = "player_entry_screen.py"
+file3 = "Splash.py"
 
 # Define functions to run the files
+
+def run_scriptSplash():
+    try:
+        subprocess.run(["python3", file3], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error running {file3}: {e}")
+
 def run_script1():
     try:
         subprocess.run(["python3", file1], check=True)
@@ -20,14 +28,17 @@ def run_script2():
 
 # Create two processes to run the scripts concurrently
 if __name__ == "__main__":
+    processSplash = multiprocessing.Process(target=run_scriptSplash)
     process1 = multiprocessing.Process(target=run_script1)
     process2 = multiprocessing.Process(target=run_script2)
 
     # Start both processes
+    processSplash.start()
     process1.start()
     process2.start()
 
     # Wait for both processes to finish
+    processSplash.join()
     process1.join()
     process2.join()
 
