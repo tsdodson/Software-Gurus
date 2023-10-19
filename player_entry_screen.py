9e2cd0ea4d6270bbd158d59e2b478d2885dff94d
William <williambfinck@gmail.com>
2023-10-19 14:07:40 -0500

    updated some info, wanted all to access

--- player_entry_screen.py
+++ player_entry_screen.py
@@ -125,7 +125,45 @@ class PlayerEntry:
     def createAction(self):
         label = tk.Label(self.frame2, text="Action Screen")
         label.grid(row=1, column=6, sticky="e")
-
+        
+        label = tk.Label(self.frame2, text= "GREEN TEAM")
+        label.grid(row= 2, column=1, sticky= "e")
+        label = tk.Label(self.frame2, text= "RED TEAM")
+        label.grid(row= 2, column=20, sticky= "e")
+       
+        for i in range(len(self.team1Entries)):
+            
+            label = tk.Label(self.frame2, text= self.team1Entries[i][1])
+            label.grid(row= 3 + i, column=1, sticky= "e")
+            label = tk.Label(self.frame2, text= self.team1Entries[i][1])
+            label.grid(row= 4 + i, column=1, sticky= "e")
+            label = tk.Label(self.frame2, text= self.team1Entries[i][1])
+            label.grid(row= 5 + i, column=1, sticky= "e")
+            label = tk.Label(self.frame2, text= self.team1Entries[i][1])
+            label.grid(row= 6 + i, column=1, sticky= "e")
+            label = tk.Label(self.frame2, text= self.team1Entries[i][1])
+            label.grid(row= 7 + i, column=1, sticky= "e")
+            label = tk.Label(self.frame2, text= self.team1Entries[i][1])
+            label.grid(row= 8 + i, column=1, sticky= "e")
+            label = tk.Label(self.frame2, text= self.team1Entries[i][1])
+            label.grid(row= 9 + i, column=1, sticky= "e")
+            label = tk.Label(self.frame2, text= self.team1Entries[i][1])
+            label.grid(row= 10 + i, column=1, sticky= "e")
+            label = tk.Label(self.frame2, text= self.team1Entries[i][1])
+            label.grid(row= 11+ i, column=1, sticky= "e")
+            label = tk.Label(self.frame2, text= self.team1Entries[i][1])
+            label.grid(row= 12 + i, column=1, sticky= "e")
+            label = tk.Label(self.frame2, text= self.team1Entries[i][1])
+            label.grid(row= 13 + i, column=1, sticky= "e")
+            label = tk.Label(self.frame2, text= self.team1Entries[i][1])
+            label.grid(row= 14 + i, column=1, sticky= "e")
+            label = tk.Label(self.frame2, text= self.team1Entries[i][1])
+            label.grid(row= 15 + i, column=1, sticky= "e")
+            label = tk.Label(self.frame2, text= self.team1Entries[i][1])
+            label.grid(row= 16 + i, column=1, sticky= "e")
+            label = tk.Label(self.frame2, text= self.team1Entries[i][1])
+            label.grid(row= 17 + i, column=1, sticky= "e")
+        
         screen_switch = tk.Button(self.frame2, text="Esc - Exit", command=self.show_entry_screen)
         screen_switch.grid(row=16, column=6)
         return
@@ -169,6 +207,8 @@ class PlayerEntry:
                 self.team2Entries.append([[self.team2ID[i].get()], [self.team2CodeName[i].get()], [self.team2EquipmentID[i].get()], [self.team2FirstName[i].get()],[self.team2LastName[i].get()]])
                 add_entries(supabase, self.team2ID[i].get(), self.team2FirstName[i].get(), self.team2LastName[i].get(), self.team2CodeName[i].get())
         self.transmit()
+        # Create action screen for frame2
+        self.createAction()
         return 
     
     # ---------------------- Clears all current entries ----------------------
@@ -228,8 +268,8 @@ class PlayerEntry:
         # Create labels and entries for frame1
         self.createEntries()
 
-        # Create action screen for frame2
-        self.createAction()
+        # # Create action screen for frame2
+        # self.createAction()
 
         # Start the main event loop
         self.root.mainloop()
